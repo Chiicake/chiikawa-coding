0f17a53eb4b46384d8e4d8fafe592bb8364133ce
Chiicake <pankaiwen1231@outlook.com>
2025-10-14 15:41:45 +0800

    user login register

---
+++ src/main/java/com/chiikawa/chiikawacoding/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.chiikawa.chiikawacoding.mapper;
+
+import com.chiikawa.chiikawacoding.model.entity.User;
+import com.mybatisflex.core.BaseMapper;
+
+/**
+ * 用户 映射层。
+ *
+ * @author <a href="https://github.com/Chiicake">chiicake</a>
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}

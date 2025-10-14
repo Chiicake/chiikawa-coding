e60af79e35bf662ab340b46eeed854ae7991e213
Chiicake <pankaiwen1231@outlook.com>
2025-10-14 15:38:41 +0800

    user login register

--- src/main/java/com/chiikawa/chiikawacoding/ChiikawaCodingApplication.java
+++ src/main/java/com/chiikawa/chiikawacoding/ChiikawaCodingApplication.java
@@ -1,9 +1,12 @@
 package com.chiikawa.chiikawacoding;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.chiikawa.chiikawacoding.mapper")
 public class ChiikawaCodingApplication {
 
     public static void main(String[] args) {

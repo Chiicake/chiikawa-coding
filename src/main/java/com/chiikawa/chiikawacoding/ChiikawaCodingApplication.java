74c67d0564de51b580a103d538d24c856f99924c
Chiicake <pankaiwen1231@outlook.com>
2025-10-14 10:43:47 +0800

    initial

---
+++ src/main/java/com/chiikawa/chiikawacoding/ChiikawaCodingApplication.java
@@ -0,0 +1,13 @@
+package com.chiikawa.chiikawacoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChiikawaCodingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChiikawaCodingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chiikawa/chiikawacoding/ChiikawaCodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chiikawa.chiikawacoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChiikawaCodingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
